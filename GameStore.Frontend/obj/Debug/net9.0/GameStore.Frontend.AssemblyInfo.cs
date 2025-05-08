92bc0d4ea2d402e9f92dc05b62ac885a4378881f
Henri Kaukoranta <henri.kaukoranta@edu.salpaus.fi>
2025-05-08 11:15:01 +0300

    games add to the list

--- GameStore.Frontend/obj/Debug/net9.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net9.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+353670b7d4c1fdcd881f5814a4319fb41da06080")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8c847318a754b4990c52958e7e24be8cd2f4cdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
